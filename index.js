54026e77f55e2a5dc74af810479369d394f02a59
venkateshabr <venkateshabr@gmail.com>
2023-04-25 19:42:05 +0530

    adding layouts for the views

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const port  = 8000;
-
 const app = express();
+const layout = require('express-ejs-layouts');
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+//We need to specify before the router about the layout
+app.use(expressEjsLayouts);
 
 //using express router
 app.use('/',require('./routes'));
