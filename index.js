228c41ad011b17578576e585f563944a446615fd
venkateshabr <venkateshabr@gmail.com>
2023-04-25 18:48:50 +0530

    home and user controller created using express router

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//using express router
+app.use('/',require('./routes'));
+
 app.listen(port,function(err){
     if(err){
         console.log("Error in listening to the port");
